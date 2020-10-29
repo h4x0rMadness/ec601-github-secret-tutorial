b7ba53de5171103a010858e3885289c244fa35d7
h4x0rMadness <robertmorrislike@gmail.com>
2020-10-29 18:31:46 -0400

    Add files via upload

---
+++ test_twitter_example.py
@@ -0,0 +1,52 @@
+import unittest
+import twitter_example
+import os
+
+
+class MyTestCase(unittest.TestCase):
+    def test_fetch_successful(self):
+        """
+        test
+        :return:
+        """
+        at = os.environ['ACCESS_TOKEN']
+        ats = os.environ['ACCESS_TOKEN_SECRET']
+        ck = os.environ['CONSUMER_KEY']
+        cs = os.environ['CONSUMER_SECRET']
+
+        res = twitter_example.fecth_random_tweets(ck, cs, at, ats)
+
+        txt = []
+        for tweet in res:
+            txt.append(tweet.text)
+
+
+
+        self.assertEqual(1, len(txt))
+
+
+    def test_empty_with_wrong_key(self):
+        """
+        Test with wrong keys, catch the exception and assert if as expected
+        :return:
+        """
+        ck = 'wrongkey'
+        cs = 'wrongkey'
+        at = 'wrongkey'
+        ats = 'wrongkey'
+
+        res = twitter_example.fecth_random_tweets(ck, cs, at, ats)
+        try:
+            for tweet in res:
+                print(tweet)
+        except Exception as e:
+            print("exception condition")
+            self.assertTrue(e, "Twitter error response: status code = 401")
+
+        # self.assertTrue(twitter_example.fecth_random_tweets(ck, cs, at, ats) == 401)
+
+        ##self.assertTrue(res.__sizeof__() == 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
