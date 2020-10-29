f2241e07639264a5081dadad5e471e68e5383aca
h4x0rMadness <robertmorrislike@gmail.com>
2020-10-29 18:09:08 -0400

    Add files via upload

---
+++ test_twitter_example.py
@@ -0,0 +1,54 @@
+import unittest
+import twitter_example
+
+
+class MyTestCase(unittest.TestCase):
+    def test_fetch_successful(self):
+        """
+        test
+        :return:
+        """
+        ck = 'rllmb3xW0SYqmv0f1fZok9q8T'
+        cs = 'w69Hk40uV9O25DjOGO36in4E4pqPpZbHASZogbB9gHfzv4r87s'
+        at = '1171844680603951104-htwoGiQZFbtEAR07GHcNmfOSQlagNi'
+        ats = 'W89apGxuxGBHX7XWL49klJQlotzGCvOychsdrovONyJiB'
+
+        res = twitter_example.fecth_random_tweets(ck, cs, at, ats)
+
+        txt = []
+        for tweet in res:
+            txt.append(tweet.text)
+
+        expecteed_txt = "Recurring #wildfires, fuelled" \
+                        " by rising #deforestation and more " \
+                        "severe drought as the planet warms, co" \
+                        "ntinue to dev… https://t.co/wUn9znVFv7"
+
+        self.assertEqual(expecteed_txt, txt[0])
+
+
+    def test_empty_with_wrong_key(self):
+        """
+        Test with wrong keys, catch the exception and assert if as expected
+        :return:
+        """
+        ck = 'wrongkey'
+        cs = 'wrongkey'
+        at = 'wrongkey'
+        ats = 'wrongkey'
+
+        res = twitter_example.fecth_random_tweets(ck, cs, at, ats)
+        try:
+            for tweet in res:
+                print(tweet)
+        except Exception as e:
+            print("exception condition")
+            self.assertTrue(e, "Twitter error response: status code = 401")
+
+        # self.assertTrue(twitter_example.fecth_random_tweets(ck, cs, at, ats) == 401)
+
+        ##self.assertTrue(res.__sizeof__() == 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
